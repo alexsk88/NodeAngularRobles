0632048e3f482e8578204bb1b54934e90011f509
alexsk88 <titoantifa69@gmail.com>
2019-06-27 17:27:57 -0500

    Primeras rutas y config de expreess

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// Cargar librerias
+    var express = require('express');
+    var bodyParser = require('body-parser');
+
+// Ejecutar Expreess
+    var app = express();
+        // Activa el Framework
+
+// Cargas archivos de Rutas
+
+// Cargar Midleware
+    app.use(bodyParser.urlencoded({extended:false}));
+        // Configuracion para parsear datos
+
+    app.use(bodyParser.json())
+    // Convierte la peticion a un Object JSON
+
+
+// Configurar CORs, para acceso cruzado entre dominios
+
+// Reescribir Rutas
+
+    app.get('/', (request, response)=>{
+         // Tiene dos funciones de Callback 
+         // La request es lo que envio 
+         // Response es lo que devuelve
+
+         return response.status(200).send({
+             messague: "Hola Marte desde el back-end con NODE",
+             nombre: "Alexnder Nova"
+         });
+    });
+
+// Exportar el modulo
+    module.exports = app;
--- index.js
+++ index.js
@@ -6,10 +6,24 @@
 var mongoose = require('mongoose');
 // Cargar libreria de Mongoose
 
+var app = require('./app');
+// Cargo la configuracion de Express
+var port = process.env.PORT || 3999;
+// Elijo un puerto, el que esta en proceso O 3999
+
+
+
 mongoose.Promise = global.Promise;
 // Le digo a moongose que utlize promesas
 
 mongoose.connect('mongodb://localhost:27017/api-rest-node', {useNewUrlParser: true})
         .then(()=>{
             console.log('La connexion a MOnGODB well DONE');
+
+            // Crear el Servidor
+            app.listen(port, ()=>
+            {
+                console.log("The Server http://localhost:3999 esta funcionando");
+            });
+
         }).catch(errro=> console.log("Aui error", error));
