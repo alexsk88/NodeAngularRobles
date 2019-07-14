4782b315b84d85f7a03b636fb13bdd7e11f83393
alexsk88 <titoantifa69@gmail.com>
2019-07-13 19:29:51 -0500

    Se add form de enviar topic

--- angularforo/src/app/panel/components/add/add.component.ts
+++ angularforo/src/app/panel/components/add/add.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Topic } from '../../../../models/topic';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UsuarioService } from '../../../services/usuario.service';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
-export class AddComponent implements OnInit {
+export class AddComponent implements OnInit
+{
+  topic:Topic;
+  identity: any;
+  token:String;
+  status: String;
 
-  constructor() { }
+  constructor(
+    private _router: Router,
+    private _Activerouter: ActivatedRoute,
+    private _userSV: UsuarioService
+  )
+  {
+    this.identity = _userSV.getIdentity();
+    this.token = _userSV.getToken();
+    this.topic = new Topic('','','','','','',this.identity._id,null);
+  }
 
-  ngOnInit() {
+  ngOnInit()
+  {
+
+  }
+
+  newTopics(form: any)
+  {
+    console.log(this.topic);
   }
 
 }
